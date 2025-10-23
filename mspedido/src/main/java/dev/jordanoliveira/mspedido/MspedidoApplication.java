242a3b6dfc23d5c86da4691390e7802c00d137c2
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2025-10-22 21:11:22 -0300

    Refactor mspedido microservice
    - Removed README.md file as it was no longer needed.
    - Updated mvnw and mvnw.cmd scripts to improve Maven wrapper functionality and fix license URL.
    - Modified pom.xml to replace spring-boot-starter-validation with spring-cloud-starter-openfeign and rearranged dependencies.
    - Cleaned up MspedidoApplication.java and MspedidoApplicationTests.java for consistent formatting.

--- mspedido/src/main/java/dev/jordanoliveira/mspedido/MspedidoApplication.java
+++ mspedido/src/main/java/dev/jordanoliveira/mspedido/MspedidoApplication.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class MspedidoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MspedidoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MspedidoApplication.class, args);
+	}
+
 }
--- mspedido/src/test/java/dev/jordanoliveira/mspedido/MspedidoApplicationTests.java
+++ mspedido/src/test/java/dev/jordanoliveira/mspedido/MspedidoApplicationTests.java
@@ -6,7 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MspedidoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
